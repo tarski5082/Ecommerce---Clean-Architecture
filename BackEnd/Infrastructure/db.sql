82b8f03991382fc4f0a87b44cd50261c28c7173e
Home <zecht@host.voo.be>
2026-08-16 16:45:52 +0200

    image produit

--- BackEnd/Infrastructure/db.sql
+++ BackEnd/Infrastructure/db.sql
@@ -140,49 +140,49 @@ INSERT INTO Categorie (Id, Nom) VALUES
 INSERT INTO Produit
     (Id, Nom, Inventaire, PrixUnitaire, ImageUrl, IdCategorie)
 VALUES
-(1, 'Ordinateur portable Lenovo IdeaPad 5', 25, 750,
- 'https://example.com/lenovo-ideapad.jpg', 1),
+('Lenovo Yoga Slim 7', 25, 750,
+ 'https://static.fnac-static.com/multimedia/Images/FR/MDM/fc/f5/a6/27719164/3756-1.jpg', 1),
 
-(2, 'MacBook Air M3', 15, 1300,
+('MacBook Air M3', 15, 1300,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/72/85/63/23299442/1540-1/tsp20260218220918/Apple-MacBook-Air-13-6-512Go-D-16-Go-RAM-Puce-M3-CPU-8-coeurs-GPU-10-coeurs-Minuit.jpg', 1),
 
-(3, 'Samsung Galaxy Tab S10 FE', 30, 750,
+('Samsung Galaxy Tab S10 FE', 30, 750,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/82/57/ab/28006274/1540-1/tsp20260619155057/Samsung-Galaxy-Tab-S10-FE-tablette-android-ecran-10-9-pouces-27-7-cm-WiFi-5-Bluetooth-5-3-RAM-12-Go-stockage-256-Go-camera-principale-13-mpx-batterie-8000-mAh-gris.jpg', 1),
 
-(4, 'Clavier mécanique Logitech G413', 40, 90,
+('Clavier mécanique Logitech G413', 40, 90,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/73/93/41/21074803/1540-1/tsp20260220031329/Clavier-Gaming-filaire-Azerty-Logitech-G-G413-SE-Noir.jpg', 5),
 
-(5, 'Souris Logitech G502', 50, 80,
+('Souris Logitech G502', 50, 80,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/4e/2a/30/19933774/1505-1/tsp20260601032040/Souris-Gaming-Sans-Fil-Logitech-G502-X-Lightspeed-pour-PC-ou-Mac-Noir.jpg', 5),
 
-(6, 'iPhone 15', 20, 800,
+('iPhone 15', 20, 800,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/a8/c7/16/1540-2/tsp20260428173815/Apple-iPhone-15-6-1-128-Go-bleu-Reconditionne-avec-Batterie-neuve.jpg#88c92110-066a-4b38-b6cc-7875a9b0ce2e', 2),
 
-(7, 'Samsung S24', 18, 900,
+('Samsung S24', 18, 900,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/a6/f6/5a/22738598/1540-1/tsp20260319135101/Smartphone-Samsung-Galaxy-S24-6-2-5G-Nano-SIM-256-Go-Noir.jpg', 2),
 
-(8, 'Google Pixel 11', 12, 800,
+('Google Pixel 11', 12, 800,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/24/34/c6/29766692/1540-1/tsp20260813091724/Smartphone-Google-Pixel-11-6-3-OLED-5G-Double-SIM-256-Go-Noir-Volcanique.jpg', 2),
 
-(9, 'Casque Sony WH-1000XM5', 22, 350,
+('Casque Sony WH-1000XM5', 22, 350,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/d9/63/25/19227609/1540-1/tsp20260612111322/Casque-audio-arceau-sans-fil-Sony-WH1000XM5-noir-a-reduction-de-bruit.jpg', 3),
 
-(10, 'AirPods Pro 2', 35, 280,
+('AirPods Pro 2', 35, 280,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/0a/e4/06/17228810/1540-1/tsp20260620100141/Apple-AirPods-Pro-2eme-generation-Blanc-avec-boitier-de-charge-MagSafe-Lightning-Ecouteurs-sans-fil-True-Wirele.jpg', 3),
 
-(11, 'Enceinte JBL Flip 6', 28, 130,
+('Enceinte JBL Flip 6', 28, 130,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/d2/2d/06/17182162/1540-1/tsp20260523184546/Enceinte-portable-etanche-sans-fil-Bluetooth-JBL-Flip-6-Bleu.jpg', 3),
 
-(12, 'PlayStation 5', 10, 550,
+('PlayStation 5', 10, 550,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/2d/8d/b7/28806445/1540-1/tsp20260730103933/Console-Sony-PS5-Slim-Edition-Standard-Blanc-et-Noir.jpg', 4),
 
-(13, 'Manette Xbox Wireless Controller', 25, 65,
+('Manette Xbox Wireless Controller', 25, 65,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/ff/be/33/20168447/1540-1/tsp20260530085138/Manette-sans-fil-Microsoft-Xbox-Elite-Series-2-Core-Blanc.jpg', 4),
 
-(14, 'Nintendo Switch OLED', 14, 350,
+('Nintendo Switch OLED', 14, 350,
  'https://static.fnac-static.com/multimedia/Images/FR/MDM/91/ea/f6/16181905/1540-1/tsp20260619170440/Nintendo-Switch-modele-OLED-avec-station-d-accueil-et-manettes-Joy-Con-blanches.jpg', 4),
 
-(15, 'Casque Gaming HyperX Cloud II', 30, 90,
+('Casque Gaming HyperX Cloud II', 30, 90,
  'https://static.fnac-static.com/multimedia/Images/60/36/9D/14/21615456-1505-1540-1/tsp20260527082658/Casque-PC-sans-fil-gaming-HyperX-Cloud-Stinger-2-Noir.jpg', 4);
 
 
